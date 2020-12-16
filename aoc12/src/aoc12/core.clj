3791bfefd10516d5492a6407a25b4bf6b56f51b9
KolorowyAleksander <kolorowyaleksander@users.noreply.github.com>
2020-12-16 20:53:16 +0100

    Add day 12 pt. 2 because i forgot earlier

--- aoc12/src/aoc12/core.clj
+++ aoc12/src/aoc12/core.clj
@@ -20,31 +20,35 @@
 
 
 ;; execute provided actions
-(defn rotate [direction d val]
-  (match d
-    "L" (mod (- direction val) 360)
-    "R" (mod (+ direction val) 360)
-    :else  (throw (Exception. (format "INVALID ROTATION in rotate: %s_%s" d val)))))
+(defn rotate [[xw yw] direction val]
+  (let [angle-deg (match direction
+                    "R" (* -1 val)
+                    "L" val
+                    :else  (throw (Exception. (format "INVALID ROTATION in rotate: %s_%s" direction val))))
+        angle (* angle-deg (/ Math/PI 180))]
+    [
+      (- 
+        (Math/round (* (Math/cos angle) xw)) 
+        (Math/round (* (Math/sin angle) yw)))
+      (+
+        (Math/round (* (Math/sin angle) xw))
+        (Math/round (* (Math/cos angle) yw)))]))
 
-(defn forward [[x y] direction val]
-  (match direction
-    0   [(+ x val) y]
-    90  [x (+ y val)]
-    180 [(- x val) y]
-    270 [x (- y val)]
-    :else  (throw (Exception. (format "INVALID DIRECTION in forward: %s_%s" direction val)))))
+(defn forward [[x y] [xw yw] val]
+  [(+ x (* xw val)) (+ y (* yw val))])
 
-(defn execute-action [coords direction action]
-  (let [[x y] coords
+(defn execute-action [coords waypoint action]
+  (let [[x y]     coords
+        [xw yw]   waypoint
         [act val] action]
     (match action
-      ["N" _] [[(+ x val) y] direction]
-      ["S" _] [[(- x val) y] direction]
-      ["E" _] [[x (+ y val)] direction]
-      ["W" _] [[x (- y val)] direction]
-      ["L" _] [[x y] (rotate direction "L" val)]
-      ["R" _] [[x y] (rotate direction "R" val)]
-      ["F" _] [(forward coords direction val) direction]
+      ["N" _] [coords [xw (+ yw val)]]
+      ["S" _] [coords [xw (- yw val)]]
+      ["E" _] [coords [(+ xw val) yw]]
+      ["W" _] [coords [(- xw val) yw]]
+      ["L" _] [coords (rotate waypoint "L" val)]
+      ["R" _] [coords (rotate waypoint "R" val)]
+      ["F" _] [(forward coords waypoint val) waypoint]
       :else  (throw (Exception. (format "INVALID ACTION in execute-action: %s_%s" act val))))))
 
 ; Action N means to move north by the given value.
@@ -57,21 +61,24 @@
 ; Rotations can only be performed in (90 180 270) degrees
 (defn execute [actions]
   (loop [coords [0 0]
-         direction 90
+         waypoint [10 1]
          actions actions
          acc []]
     (if (empty? actions)
       acc
       (let [f (first actions)
-            [new_coords new_direction] (execute-action coords direction f)]
+            [new_coords new_waypoint] (execute-action coords waypoint f)]
         (recur 
           new_coords 
-          new_direction 
+          new_waypoint 
           (drop 1 actions)
-          (conj acc [new_coords new_direction]))))))
+          (conj acc [new_coords new_waypoint]))))))
 
 (defn calculate [[[x y] _]]
   (+ (math/abs x) (math/abs y)))
+
+(defn onetwothree [seq]
+  (doseq [x seq] (prn x)))
 
 ;; entrypoint
 (defn -main
@@ -83,3 +90,4 @@
     (last)
     (calculate)
     (prn)))
+    ; (onetwothree)))
